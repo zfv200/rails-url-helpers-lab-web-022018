387273d3c72612e90927c4039b6a9fa15fffb41c
github username <github email address>
2018-03-17 16:47:16 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,13 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active?
+    self.active == false ? "This student is currently inactive." : "This student is currently active."
+  end
+
+  def activate
+    self.active = !self.active
+    self.save
+  end
 end
